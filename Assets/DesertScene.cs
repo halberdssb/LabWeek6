f8c157f036e3a0458e8d1d2e324b71cda7dcb026
halberdssb <jwstevenson22@gmail.com>
2025-02-13 12:00:38 -0500

    pyramid script working

---
+++ Assets/DesertScene.cs
@@ -0,0 +1,120 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DesertScene : MonoBehaviour
+{
+    private Vector2 sceneDimensions; // size of plane/scene area
+
+    // forest variables
+    private int numForestObjects; // number of objects to make in the forest
+    private float minForestObjectSize;
+    private float maxForestObjectSize;
+
+    // pyramid variables
+    private int numPyramidRows; // how many levels the pyramid will have
+    private float spaceBtwCubes; // space buffer between cubes to allow spac ebetween everything
+    private Color[] pyramidColors; // colors for each pyramid layer
+    private GameObject pyramidParent; // parent object for pyramid cubes
+    private Vector3 pyramidCenter; // center position of pyramid
+
+
+    void Start()
+    {
+        InitializeVariables();
+        CreateGround();
+        CreatePyramid();
+    }
+
+    // sets variables for scene creation
+    public void InitializeVariables()
+    {
+        // set size of scene to 20 x and 20 y
+        sceneDimensions = new Vector2(1, 1);
+
+        // get random number of forest objects from 5 to 10
+        numForestObjects = Random.Range(5, 11);
+
+        // set pyramid levels to 5
+        numPyramidRows = 5;
+
+        // set pyramid colors for each tier
+        pyramidColors = new Color[numPyramidRows];
+        pyramidColors[0] = Color.red;
+        pyramidColors[1] = Color.yellow;
+        pyramidColors[2] = Color.green;
+        pyramidColors[3] = Color.blue;
+        pyramidColors[4] = Color.magenta;
+
+        // create empty parent object
+        pyramidParent = new GameObject("PyramidParent");
+
+        // get center point of pyramid
+        pyramidCenter = new Vector3(2, 0.5f, -3);
+    }
+
+    // creates the ground plane for the scene
+    public void CreateGround()
+    {
+        // create plane
+        GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+        // set size of plane to ground dimensions - keep y scale the same to have thin ground layer
+        ground.transform.localScale = new Vector3(sceneDimensions.x, ground.transform.localScale.y, sceneDimensions.y);
+
+        // set color of plane to red
+        SetObjectColor(ground, Color.red);
+
+
+    }
+
+    public void CreatePyramid()
+    {
+        // used to offset each layer of pyramid on middle
+        float offset = 0;
+
+        // loop through each level of pyramid
+        for (int i = numPyramidRows; i > 0; i--)
+        {
+            // calculate offset for each row - 0.5 is half cube width
+            offset = (numPyramidRows - i) * 0.5f;
+
+            // loop through each row in level
+            for (int j = i; j > 0; j--)
+            {
+                // create cube on each space of the row
+                for (int k = i; k > 0; k--)
+                {
+                    // create object
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    // move to desired position and scale down slightly to create offset
+                    cube.transform.position = new Vector3(k + offset, 5 - i, j + offset);
+                    cube.transform.position += pyramidCenter;
+                    cube.transform.localScale *= 0.9f;
+
+                    // set parent of cube to clean up hierarchy
+                    cube.transform.parent = pyramidParent.transform;
+
+                    // set color of cube
+                    SetObjectColor(cube, pyramidColors[numPyramidRows - i]);
+                }
+            }
+        }
+    }
+
+    public void CreateForest()
+    {
+
+    }
+
+    // creates a new material of a desired color and sets it to the object passed in
+    private void SetObjectColor(GameObject primitive, Color color)
+    {
+        Renderer renderer = primitive.GetComponent<Renderer>();
+        renderer.material = new Material(Shader.Find("Standard"));
+        renderer.material.color = color;
+    }
+}
